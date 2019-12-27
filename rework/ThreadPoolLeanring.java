3470488429c5aa2c2262be5f5992bdcfc4cf8fe2
Ivana_ShineMo <login.ivanna@gmail.com>
2019-12-27 18:12:21 +0800

    retry of ThreadPoolExecutor, something wrong with multi-Producer-Consuemr. 20191227

--- rework/ThreadPoolLeanring.java
+++ rework/ThreadPoolLeanring.java
@@ -1,65 +1,47 @@
 package rework;
 
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.*;
 
 public class ThreadPoolLeanring{
 	public static void main(String[] args) {
-//		UseArrayBlockingQ useArrayBlockingQ = new UseArrayBlockingQ(2, 5, 30);
-//		useArrayBlockingQ.produceAndConsume();
+		//create workQueue
+		ArrayBlockingQueue<Object> tlist = new ArrayBlockingQueue<>(30);
 		
-		ThreadPoolExecutorLearning threadPoolExecutorLearning = new ThreadPoolExecutorLearning(5,2);
-		ThreadPoolExecutor threadPool = threadPoolExecutorLearning.FixedPool();
-		ArrayBlockingQueue<Integer> tlist = new ArrayBlockingQueue<>(30);
- 		Runnable producer1= new PCFactory().getPC("Producer",tlist);
- 		Runnable consumer1 = new PCFactory().getPC("consumer", tlist);
- 		Runnable consumer2 = new PCFactory().getPC("consumer", tlist);
- 		threadPool.execute(producer1);
- 		threadPool.execute(consumer1);
- 		threadPool.execute(consumer2);
-	} 
+		// create single-thread thread pool
+//		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1,1,0L,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(5));
+//		// create fixed thread pool
+//		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,5,0L,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(5));
+//		// create cached thread pool
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,Integer.MAX_VALUE,0L,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(5));
+		
+		PCFactory pcFactory = new PCFactory();
+		Runnable myProducer1 = pcFactory.getPC("producer",tlist);
+		Runnable myConsumer1 = pcFactory.getPC("Consumer",tlist);
+		Runnable myConsumer2 = pcFactory.getPC("Consumer",tlist);
+		
+		threadPoolExecutor.execute(myProducer1);
+		threadPoolExecutor.execute(myConsumer1);
+		threadPoolExecutor.execute(myConsumer2);
+	}
 }
 
 
 class PCFactory{
-	public Runnable getPC(String type, ArrayBlockingQueue<Integer> tlist) {
+	public Runnable getPC(String type, ArrayBlockingQueue<Object> tlist) {
 		if (type.equalsIgnoreCase("producer")){
-			return new PLeaningProducer(tlist);
+			return new MyProducer(tlist);
 		}else {
-			return new PLeaningConsumer(tlist);
+			return new MyConsumer(tlist);
 		}
-	}
-}
-
-class ThreadPoolExecutorLearning{
-	int taskNum=1;
-	int threadNum=1;
-	
-	public ThreadPoolExecutorLearning(int taskNum,int threadNum) {
-		this.taskNum = taskNum;
-		this.threadNum = threadNum;
-	}
-	public ThreadPoolExecutor StaticPool() {
-		ThreadPoolExecutor singleThreadTP = new ThreadPoolExecutor(1,1,0L,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(taskNum));
-		return singleThreadTP;
-	} 
-	public ThreadPoolExecutor FixedPool() {
-		ThreadPoolExecutor fixedThreadTP = new ThreadPoolExecutor(threadNum,threadNum,0L,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(taskNum));
-		return fixedThreadTP;
-	}
-	public ThreadPoolExecutor CachedPool() {
-		ThreadPoolExecutor CachedThreadTP = new ThreadPoolExecutor(0,Integer.MAX_VALUE,0L,TimeUnit.MILLISECONDS,new ArrayBlockingQueue<Runnable>(taskNum));
-		return CachedThreadTP;
 	}
 }
 
 
 class MyProducer implements Runnable {
-	private ArrayBlockingQueue<Integer> tlist;
+	private ArrayBlockingQueue<Object> tlist;
 	private int e = 0;
 	
-	public MyProducer(ArrayBlockingQueue<Integer> tlist) {
+	public MyProducer(ArrayBlockingQueue<Object> tlist) {
 		this.tlist = tlist;
 	}
 	
@@ -72,8 +54,6 @@ class MyProducer implements Runnable {
 	
 	//TODO: How to let the limit work
 	public void producer(){
-//		int e =  new Random().nextInt(1000);
-//		System.out.println("Produce: "+this.e);
 		try {
 			tlist.put(e);
      		System.out.println(Thread.currentThread().getName()+" has put: "+e);
@@ -86,26 +66,25 @@ class MyProducer implements Runnable {
 
 
 class MyConsumer implements Runnable{
-	private ArrayBlockingQueue<Integer> tlist;
+	private ArrayBlockingQueue<Object> tlist;
 	
-	public MyConsumer(ArrayBlockingQueue<Integer> tlist) {
+	public MyConsumer(ArrayBlockingQueue<Object> tlist) {
 		this.tlist = tlist;
 	}
 	
 	@Override
 	public void run() {
 		while (true) {
-			consumer();	   //not need to add sleep() here or in run() of producer to avoid deadlock
+			consumer();	 
 		}
 	}
 	
 	public void consumer(){
 		try {
-		    Integer value = tlist.take();
+			Object value = tlist.take();
 			System.out.println(Thread.currentThread().getName()+" has got: "+value);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-
 }
